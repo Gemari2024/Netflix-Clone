248896ba7750be7cb8c61fc7166af014540e709b
Gemari2024 <tnebyat678@gmail.com>
2024-09-07 20:34:03 -0400

    banner

---
+++ netflix-clone/src/components/banner/Banner.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import instance from '../../utils/axios'
+import requests from '../../utils/requests'
+import axios from "axios";
+function Banner() {
+  const [movie, setMovie] = useState({});
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        console.log(request)
+        setMovie(request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ]);
+      } catch (error) {
+        // console.log("error", error);
+      }
+    })();
+  }, []);
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage:
+          "url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')",
+        backgroundPosition: "center",
+        backgroundRepeat: "non-repreat",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.orginal_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button play">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {/* {truncation(movie?.overview, 150)} */}
+        </h1>
+      </div>
+      <div className="banner_fadeBottom" />
+    </div>
+  );
+}
+
+export default Banner;
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,13 +1,22 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
-  fetchTrending: "/trending/all/week?api_key=${API_KEY}&language=en-US",
-  fetchNetflixOriginals: "/discover/tv?api_key=${API_KEY}&with_networks=213",
-  fetchTopRatedMovies: "/movie/top_rated?api_key=${API_KEY}&language=en-US",
-  fetchActionMovies: "/discover/movie?api_key=${API_KEY}&with_genres=28",
-  fetchComedyMovies: "/discover/movies?api_key=${API_KEY}&with_genres=35",
-  fetchHorrorMovies: "/discover/movies?api_key=${API_KEY}&with_genres27",
-  fetchRomanceMovies: "/discover/movies?api_key=${API_KEY}&with_genres=10749",
-  fetchDocumentaries: "/discover/movies?api_key=${API_KEY}&with_genres=99",
-  fetchTvShow: "tv/popular?api_key=${API_KEY}&language=en-US&page=1",
+  fetchTrending:
+    "/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&language=en-US",
+  fetchNetflixOriginals:
+    "/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_networks=213",
+  fetchTopRatedMovies:
+    "/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US",
+  fetchActionMovies:
+    "/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=28",
+  fetchComedyMovies:
+    "/discover/movies?api_key=${process.env.REACT_APP_API_KEY}&with_genres=35",
+  fetchHorrorMovies:
+    "/discover/movies?api_key=${process.env.REACT_APP_API_KEY}&with_genres27",
+  fetchRomanceMovies:
+    "/discover/movies?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10749",
+  fetchDocumentaries:
+    "/discover/movies?api_key=${process.env.REACT_APP_API_KEY}&with_genres=99",
+  fetchTvShow:
+    "tv/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1",
 };
 export default requests
