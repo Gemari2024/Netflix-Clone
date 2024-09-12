d61028abd3713bbce277ffba7deca2655ffe15bc
Gemari2024 <tnebyat678@gmail.com>
2024-09-11 21:56:19 -0400

    config

--- netflix-clone/src/components/Rows/Row/Row.jsx
+++ netflix-clone/src/components/Rows/Row/Row.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./row.css";
 import axios from "../../../utils/axios";
-import movieTrailer from 'movie-trailer'
+import movieTrailer from "movie-trailer";
 // import YouTube from "@mui/icons-material/YouTube";
-import YouTube from 'react-youtube'
+import YouTube from "react-youtube";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
@@ -26,8 +26,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      movieTrailer(movies?.title || movies?.name || movies?.original_name)
-      .then((url) => {
+      movieTrailer(movies?.title || movies?.name || movies?.original_name).then(
+        (url) => {
           console.log(url);
           const urlParams = new URLSearchParams(new URL(url).search);
           console.log(urlParams);
@@ -49,28 +49,27 @@ function Row({ title, fetchUrl, isLargeRow }) {
       <div className="row">
         <h1>{title}</h1>
         <div className="row_posters">
-          {movies?.map((movie, index) => (
-            const imagePath =LargeRow? movie.poster_path 
-            return imagePath? (
-            <img
-              onClick={()=>handleClick(movie)}
-              key={index}
-              src={`${base_url}${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-              alt={movie.name}
-              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            />
-            ): null
-          ))}
+          {movies?.map((movie, index) => {
+            const imagePath = isLargeRow
+              ? movie.poster_path
+              : movie.backdrop_path;
+            return imagePath ? (
+              <img
+                onClick={() => handleClick(movie)}
+                key={index}
+                src={`${base_url}${imagePath}`}
+                alt={movie.name || "movie image"}
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              />
+            ) : null;
+          })}
         </div>
         <dive style={{ padding: "40px" }}>
-          {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
+          {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </dive>
       </div>
     </>
   );
-  
 }
 
 export default Row;
