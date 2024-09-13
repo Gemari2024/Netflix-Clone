af0a28b0acfb60ba644eac4c2b81c108ae917bb9
Gemari2024 <tnebyat678@gmail.com>
2024-09-13 00:46:45 -0400

    update

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import axios from '../../utils/axios'
-import requests from '../../utils/requests'
-import './banner.css'
+import axios from "../../utils/axios";
+import requests from "../../utils/requests";
+import "./banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -12,16 +12,18 @@ function Banner() {
       try {
         const request = await axios.get(requests.fetchNetflixOriginals);
         // console.log(request)
-        setMovie(request.data.results[
+        setMovie(
+          request.data.results[
             Math.floor(Math.random() * request.data.results.length)
-          ]);
+          ]
+        );
       } catch (error) {
         console.log("error", error);
       }
     })();
   }, []);
-  function truncate(str, n){
-    return str?.length > n ? str.substr(0,n-1) + '...':str;
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
   // console.log(movie)
   return (
@@ -29,10 +31,9 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage:
-          `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
         backgroundPosition: "center",
-        backgroundRepeat: "non-repreat",
+        backgroundRepeat: "non-repeat",
       }}
     >
       <div className="banner_contents">
@@ -43,11 +44,10 @@ function Banner() {
           <button className="banner_button play">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">
-          {truncate(movie?.overview, 150)}
-        </h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className="banner_fadeBottom" />
+      {/* <div /> */}
     </div>
   );
 }
